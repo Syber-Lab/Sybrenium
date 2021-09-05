ba00dc291942e0c448e32bde2e75b1d390fca8bf
Yaseen <fantasticyaseenshariff@gmail.com>
2021-09-05 13:20:06 +0530

    update js links

--- dist/js/master.js
+++ dist/js/master.js
@@ -1,15 +1,22 @@
+
+
 function include(file) {
 
-var script = document.createElement('script');
-script.src = file;
-script.type = 'text/javascript';
-script.defer = true;
+  var script  = document.createElement('script');
+  script.src  = file;
+  script.type = 'text/javascript';
+  script.defer = true;
 
-document.getElementsByTagName('head').item(0).appendChild(script);
+  document.getElementsByTagName('head').item(0).appendChild(script);
 
 }
 
 /* Include Many js files */
-include('jquery.draggableTouch.js');
-include('https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js');
-include('myscriptfunctional.js');
+// include('https://cdn.jsdelivr.net/gh/Yaseen549/js-test-case-sybrenium@main/js/main.js');
+// include('https://cdn.jsdelivr.net/gh/Yaseen549/js-test-case-sybrenium@main/js/second.js');
+
+include('main.js');
+include('second.js');
+
+
+// alert("Am an alert");
