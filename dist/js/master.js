09c90f331ce236aab681209741b3d848d752cf74
Yaseen <fantasticyaseenshariff@gmail.com>
2021-09-05 13:31:22 +0530

    js include link update

--- dist/js/master.js
+++ dist/js/master.js
@@ -1,5 +1,3 @@
-
-
 function include(file) {
 
   var script  = document.createElement('script');
@@ -8,15 +6,9 @@ function include(file) {
   script.defer = true;
 
   document.getElementsByTagName('head').item(0).appendChild(script);
-
 }
 
 /* Include Many js files */
-// include('https://cdn.jsdelivr.net/gh/Yaseen549/js-test-case-sybrenium@main/js/main.js');
-// include('https://cdn.jsdelivr.net/gh/Yaseen549/js-test-case-sybrenium@main/js/second.js');
-
-include('main.js');
-include('second.js');
-
-
-// alert("Am an alert");
+include('https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js');
+include('https://cdn.jsdelivr.net/gh/Yaseen549/js-test-case-sybrenium@main/js/main.js');
+include('https://cdn.jsdelivr.net/gh/Yaseen549/js-test-case-sybrenium@main/js/second.js');
