a3898fd610f1c8aac95ced11c2c27e96b0a781be
Yaseen <fantasticyaseenshariff@gmail.com>
2022-06-21 11:54:48 +0530

    test js

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,12 +1,13 @@
-// function include(file) {
-//
-//   var script  = document.createElement('script');
-//   script.src  = file;
-//   script.type = 'text/javascript';
-//   script.defer = true;
-//
-//   document.getElementsByTagName('head').item(0).appendChild(script);
-// }
-//
-// /* Include Many js files */
-// include("");
+function include(file) {
+
+  var script  = document.createElement('script');
+  script.src  = file;
+  script.type = 'text/javascript';
+  script.defer = true;
+
+  document.getElementsByTagName('head').item(0).appendChild(script);
+}
+
+/* Include Many js files */
+include("https://cdn.jsdelivr.net/npm/sybrenium-js@0.0.0/sybrenium-js/sybrenium.js");
+include("https://cdn.jsdelivr.net/npm/sybrenium-js@0.0.0/sybrenium-js/animations.js")
