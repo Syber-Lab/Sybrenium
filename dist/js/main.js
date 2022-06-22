bd5c626db55fbc4e860ff8edcddf2c6da4ae5d0e
Yaseen <fantasticyaseenshariff@gmail.com>
2022-06-22 18:00:30 +0530

    js .5

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,4 @@
+
 function include(file) {
 
   var script  = document.createElement('script');
@@ -9,5 +10,4 @@ function include(file) {
 }
 
 /* Include Many js files */
-include("https://cdn.jsdelivr.net/npm/sybrenium-js@0.0.0/sybrenium-js/sybrenium.js");
-include("https://cdn.jsdelivr.net/npm/sybrenium-js@0.0.0/sybrenium-js/animations.js")
+include("https://cdn.jsdelivr.net/npm/sybrenium-js@0.0.5/sybrenium-js/sybrenium.js");
